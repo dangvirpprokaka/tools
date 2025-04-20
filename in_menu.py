d0d08ff685ab10dcab278cda1faaddcda6af6e89
dangvirpprokaka <letronghaidang07@gmail.com>
2025-04-20 16:47:51 +0700

    Create in_menu.py

---
+++ in_menu.py
@@ -0,0 +1,25 @@
+    print(f"{trang}╔" + "─" * width + "╗")
+    print(f"{trang}|{xnhac}{'Golike'.center(width)}{trang}|")
+    print(f"{trang}╚" + "─" * width + "╝")
+    for key in ["1.1", "1.2"]:
+        print(f'{hdang}Nhập {do}[{vang}{key}{do}] {luc}{tool_map[key][0]}')
+
+    print(f"{trang}╔" + "─" * width + "╗")
+    print(f"{trang}|{xnhac}{'Trao Đổi Sub'.center(width)}{trang}|")
+    print(f"{trang}╚" + "─" * width + "╝")
+    for key in ["2.1", "2.2", "2.3", "2.4"]:
+        print(f'{hdang}Nhập {do}[{vang}{key}{do}] {luc}{tool_map[key][0]}')
+
+    print(f"{trang}╔" + "─" * width + "╗")
+    print(f"{trang}|{xnhac}{'Tương Tác Chéo'.center(width)}{trang}|")
+    print(f"{trang}╚" + "─" * width + "╝")
+    for key in ["3.1", "3.2", "3.3", "3.4"]:
+        print(f'{hdang}Nhập {do}[{vang}{key}{do}] {luc}{tool_map[key][0]}')
+
+    print(f"{trang}╔" + "─" * width + "╗")
+    print(f"{trang}|{xnhac}{'Facebook'.center(width)}{trang}|")
+    print(f"{trang}╚" + "─" * width + "╝")
+    for key in ["4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8"]:
+        print(f'{hdang}Nhập {do}[{vang}{key}{do}] {luc}{tool_map[key][0]}')
+
+    print(f"{trang}" + "─" * 57)
